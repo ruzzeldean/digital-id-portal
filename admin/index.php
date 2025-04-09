9e9298d69ceb91835647d730dbef0f3e83616431
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-09 11:40:27 +0800

    Login form

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<?php include './includes/header.php'; ?>
+  <link rel="stylesheet" href="./css/style.css">
+<?php include './includes/header-close.php'; ?>
+  
+    <main class="d-flex justify-content-center align-items-center">
+      <div class="login-form bg-light shadow rounded d-flex flex-column p-4">
+        <h1 class="mb-4">Login</h1>
+
+        <input class="form-control mb-3" type="text" name="username" id="username" placeholder="Username">
+
+        <input class="form-control mb-3" type="password" name="password" id="password" placeholder="Password">
+
+        <button class="btn btn-primary mb-3">Login</button>
+      </div>
+    </main>
+
+<?php include './includes/footer.php'; ?>
