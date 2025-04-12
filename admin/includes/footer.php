b07ac5311010c57f1f6da910e47ca94398d9703b
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-12 22:22:33 +0800

    Change line position of jquery and bootstrap

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
-  <script src="../assets/js/bootstrap.bundle.min.js"></script>
   <script src="../assets/js/jquery.js"></script>
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
   <script src="./js/script.js"></script>
 </body>
 </html>
