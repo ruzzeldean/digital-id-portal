31b393fc66ee3470c901a7eb734ca49a414e97f8
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-13 10:11:17 +0800

    Modify link to bootstrap icons

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin | Digital ID Portal</title>
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../assets/css/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="../assets/bootstrap-icons/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="../assets/css/dataTables.bootstrap5.css">
   <link rel="stylesheet" href="../assets/css/buttons.bootstrap5.css">
   <link rel="stylesheet" href="./css/style.css">
