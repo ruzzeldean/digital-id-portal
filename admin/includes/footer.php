052bcb88d3de2825dad97fc5e72cae5b61c45d1b
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-13 09:14:44 +0800

    Create another footer with table scripts

---
+++ admin/includes/footer.php
@@ -0,0 +1,15 @@
+  <script src="../assets/js/jquery.js"></script>
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/dataTables.js"></script>
+  <script src="../assets/js/dataTables.bootstrap5.js"></script>
+  <script src="../assets/js/dataTables.buttons.js"></script>
+  <script src="../assets/js/buttons.bootstrap5.js"></script>
+  <script src="../assets/js/jszip.min.js"></script>
+  <script src="../assets/js/pdfmake.min.js"></script>
+  <script src="../assets/js/vfs_fonts.js"></script>
+  <script src="../assets/js/buttons.html5.min.js"></script>
+  <script src="../assets/js/buttons.print.min.js"></script>
+  <script src="../assets/js/buttons.colVis.min.js"></script>
+  <script src="./js/script.js"></script>
+</body>
+</html>
