31c1bfb3d5dcb1f8cfe4d20971766554637c8256
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-13 08:46:21 +0800

    Link bootstrap icons

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -5,5 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin | Digital ID Portal</title>
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/bootstrap-icons.min.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
