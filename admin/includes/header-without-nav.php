768cefe5923fe88c41b1cb0bbbf8600fad6db1d7
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-14 15:12:54 +0800

    Fix bootstrap icon css link

--- admin/includes/header-without-nav.php
+++ admin/includes/header-without-nav.php
@@ -5,6 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin | Digital ID Portal</title>
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../assets/css/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="../assets/bootstrap-icons/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
