3d7e36b0db4f915955e7d7d5789a54cfa6a1abf1
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-18 12:36:36 +0800

    Move script for login to index.php

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,32 +1,3 @@
-// Login script
-$(document).on('click', '#login-button', function() {
-  let username = $("#username").val();
-  let password = $("#password").val();
-
-  if (username === "") {
-    alert("Username is required.");
-  } else if (password === "") {
-    alert("Password is required.");
-  } else {
-    $.ajax({
-      url: './login.php',
-      method: 'POST',
-      data: {
-        username: username,
-        password: password
-      },
-      success: function(response) {
-        if (response == "success") {
-          alert("Access granted.");
-          location.href="./dashboard.php";
-        } else {
-          alert("Access denied.");
-        }
-      }
-    })
-  }
-})
-
 // Page title script
 $(document).ready(function () {
   $("nav a").on("click", function () {
