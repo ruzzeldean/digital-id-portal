8c324e658bc33984f0e6fe6ec389c76450de80c5
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-23 11:09:07 +0800

    Implement a 404 error page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+  http_response_code(404);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 | Page Not Found</title>
+  <link rel="stylesheet" href="/digital-id-portal/assets/css/bootstrap.min.css">
+</head>
+<body class="p-3">
+  <h1>Error: 404 - Page Not Found</h1>
+  <p>Sorry, the page you are looking for does not exist.</p>
+  <a class="btn btn-primary" href="./">Return to Home</a>
+
+  <script src="/digital-id-portal/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
