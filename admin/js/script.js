82ac510a13c11fbc1b83256d5675f75f4d275487
Ruzzel Dean <ruzzelparungao.xyz@gmail.com>
2025-04-26 10:57:00 +0800

    Create add and edit script for admin

--- admin/js/script.js
+++ admin/js/script.js
@@ -32,16 +32,112 @@ new DataTable('.table-list', {
   }
 });
 
+// Script for adding new admin
+$(document).on('click', '#add-admin-button', () => {
+  const username = $("#username").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirm-password").val();
+  const firstName = $("#first-name").val();
+  const middleName = $("#middle-name").val();
+  const lastName = $("#last-name").val();
+  const role = $("#role").val();
+
+  if (username.trim() === "") {
+    alert("Username is required.");
+  } else if (password.trim() === "") {
+    alert("Password is required.");
+  } else if (password.length < 6) {
+    alert("Password must be more than 6 characters.");
+  } else if (confirmPassword.trim() === "") {
+    alert("Confirm password is required.");
+  } else if (password.trim() !== confirmPassword.trim()) {
+    alert("Password do not match.");
+  } else if (firstName.trim() === "") {
+    alert("First name is required.");
+  } else if (lastName.trim() === "") {
+    alert("Last name is required.");
+  } else if (role === null) {
+    alert("Role is required.");
+  } else {
+    $.ajax({
+      url: './save-admin.php',
+      method: 'POST',
+      data: {
+        username: username,
+        password: password,
+        firstName: firstName,
+        middleName: middleName,
+        lastName: lastName,
+        role: role
+      },
+      success: (response) => {
+        alert(response);
+        location.reload();
+        location.href='./admins.php';
+      }
+    });
+  }
+});
+
+// Script for editing admin
+$(document).on('click', '#edit-admin-button', function() {
+  const editId = $(this).attr('edit-id');
+  const username = $("#username").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirm-password").val();
+  const firstName = $("#first-name").val();
+  const middleName = $("#middle-name").val();
+  const lastName = $("#last-name").val();
+  const role = $("#role").val();
+
+  if (username.trim() === "") {
+    alert("Username is required.");
+  } else if (password.trim() === "") {
+    alert("Password is required.");
+  } else if (password.length < 6) {
+    alert("Password must be more than 6 characters.");
+  } else if (confirmPassword.trim() === "") {
+    alert("Confirm password is required.");
+  } else if (password.trim() !== confirmPassword.trim()) {
+    alert("Password do not match.");
+  } else if (firstName.trim() === "") {
+    alert("First name is required.");
+  } else if (lastName.trim() === "") {
+    alert("Last name is required.");
+  } else if (role === null) {
+    alert("Role is required.");
+  } else {
+    $.ajax({
+      url: './update-admin.php',
+      method: 'POST',
+      data: {
+        editId: editId,
+        username: username,
+        password: password,
+        firstName: firstName,
+        middleName: middleName,
+        lastName: lastName,
+        role: role
+      },
+      success: (response) => {
+        alert(response);
+        location.reload();
+        location.href='./admins.php';
+      }
+    });
+  }
+});
+
 // Delete admin script
-$(document).on('click', '.delete-admin', function () {
-  let id = $(this).attr('id');
+$(document).on('click', '.delete-admin', function() {
+  const adminId = $(this).attr('admin-id');
 
   $.ajax({
     url: './delete-admin.php',
     method: 'POST',
     data: {
-      id: id
-    }, success: function(response) {
+      adminId: adminId
+    }, success: function() {
       // Old style
       /* alert(response);
       location.reload(); */
@@ -53,9 +149,9 @@ $(document).on('click', '.delete-admin', function () {
       $('#successModal').modal('show');
 
       // Reload page after closing the success modal
-      $('#successModal').on('hidden.bs.modal', function () {
+      $('#successModal').on('hidden.bs.modal', () => {
         location.reload();
       })
     }
-  })
-})
+  });
+});
