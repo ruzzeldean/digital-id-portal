639ad7093121f99c3e3c95671223a389696f164b
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-05-20 16:54:59 +0800

    Modify whatever it is

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Digital ID Portal</title>
-  <link rel="stylesheet" href="./assets/css/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -47,7 +47,7 @@
       </div>
     </main>
   </div>
-  <script src="./assets/js/jquery.js"></script>
+  <script src="./assets/js/jquery.min.js"></script>
   <script src="./assets/js/bootstrap.bundle.min.js"></script>
   <script src="./js/script.js"></script>
 </body>
