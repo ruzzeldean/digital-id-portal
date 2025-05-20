d0726af6d1dca812b90333c293b28c52aa23d924
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-05-20 16:54:37 +0800

    Modify citizen account

--- citizen/index.php
+++ citizen/index.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['gatepass'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Citizen | Digital ID Portal</title>
-  <link rel="stylesheet" href="../assets/css/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/style.css">
 </head>
@@ -74,7 +74,7 @@ if (!isset($_SESSION['gatepass'])) {
       </div>
     </footer>
   </div>
-  <script src="../assets/js/jquery.js"></script>
+  <script src="../assets/js/jquery.min.js"></script>
   <script src="../assets/js/bootstrap.bundle.min.js"></script>
   <script src="../js/script.js"></script>
 </body>
